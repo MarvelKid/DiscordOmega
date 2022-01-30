23f74babcf9de9648eed3f113eba0999636a1196
MarvelKid <91361096+MarvelKid@users.noreply.github.com>
2022-01-30 10:20:16 +0000

    nothing much

--- index.js
+++ index.js
@@ -15,21 +15,28 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
-  client.user.setStatus('idle');
+//  client.user.setStatus('idle');
+  client.user.setPresence({ activity: { name: "o!help" }, status: "idle"} )
 	console.log('Bot is online');
   const channel = client.channels.cache.get('935144122019373086');
   channel.send('I am online');
 });
 
-client.on('message', async msg => {
-  switch (msg.content) {
-    case "o!ping":
-      msg.reply("Pong!");
-      break;
+client.on('message', async message => {
 
-    case "o!test":
-      msg.reply("You are one of the first to test the bot and have received the <@&928698378135674920> role")
-   }
+if(message.content === "ping"){
+  message.channel.send("pong")
+}
+
+
+
 })
 
 client.login(process.env.token);
+
+/*smth
+
+
+
+jjhu
+help*/
