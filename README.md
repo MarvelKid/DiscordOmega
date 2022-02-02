a6d2505df099970be205bfabf844bc9ae6854fdd
MarvelKid <91361096+MarvelKid@users.noreply.github.com>
2022-02-02 16:33:54 +0000

    ban + kick not stable

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # DiscordOmega
 The version 3.1.0 of omega bot
 Can't do much now, But stay tuned
+#New Updates!
+
--- index.js
+++ index.js
@@ -15,19 +15,40 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
-//  client.user.setStatus('idle');
-  client.user.setPresence({ activities: [{ name: 'o!help' }], status: 'online' });
+//   client.user.setStatus('idle');
+  client.user.setPresence({ activities: [{ name: 'o!help' }], status: 'idle' });
 	console.log('Bot is online');
   const channel = client.channels.cache.get('935144122019373086');
   channel.send('I am online');
 });
 
-client.on('message', async message => {
-
+client.on('message', message => {
+  const user = message.mentions.users.first();
 if(message.content === "ping"){
   message.channel.send("pong")
-}
+} else if (message.content.startsWith("o!ban")) {if (message.member.hasPermission("BAN_MEMBERS")) {
 
-})
+  
+    if (user) {
+        try {
+          mesasge.guild.members.ban(user);
+        } catch {
+            message.reply("I do not have permissions to ban" + user);
+        }
+    } else {
+        message.reply("You do not have permissions to ban" + user);
+    }
+}} else if (message.content.startsWith("o!kick")) {if (message.member.hasPermission("KICK_MEMBERS")) {
+    if (user) {
+        try {
+            user.kick();
+        } catch {
+            message.reply("I do not have permissions to kick " + user);
+        }
+    } else {
+        message.reply("You do not have permissions to kick " + user);
+    }
+}}
+});
 
 client.login(process.env.token);
